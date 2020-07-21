19fe1880aef0a64967db26f56ff5ce6ff47d347f
Luca Kaufmann <lucakaufmann92@gmail.com>
2020-07-21 14:15:33 +0300

    fixing foreign keys

--- Sources/App/Modules/Content/Migrations/ContentMigrations_v1_0_0.swift
+++ Sources/App/Modules/Content/Migrations/ContentMigrations_v1_0_0.swift
@@ -20,12 +20,12 @@ struct ContentMigrations_v1_0_0: Migration {
                 .field(SubtopicModel.FieldKeys.title, .string, .required)
                 .field(SubtopicModel.FieldKeys.filters, .array(of: .string), .required)
                 .field(SubtopicModel.FieldKeys.subfilters, .array(of: .string), .required)
-                .field(SubtopicModel.FieldKeys.topicId, .uuid, .required, .references(TopicModel.schema, .id))
+                .field(SubtopicModel.FieldKeys.topicId, .uuid, .required)
 //                .unique(on: SubtopicModel.FieldKeys.subtopicId)
-//                .foreignKey(SubtopicModel.FieldKeys.topicId,
-//                    references: TopicModel.schema, TopicModel.FieldKeys.topicId,
-//                    onDelete: .cascade,
-//                    onUpdate: .cascade)
+                .foreignKey(SubtopicModel.FieldKeys.topicId,
+                            references: TopicModel.schema, .id,
+                    onDelete: .cascade,
+                    onUpdate: .cascade)
                 .create(),
             db.schema(VideoModel.schema)
                 .id()
@@ -34,11 +34,11 @@ struct ContentMigrations_v1_0_0: Migration {
                 .field(VideoModel.FieldKeys.tags, .array(of: .string), .required)
                 .field(VideoModel.FieldKeys.description, .string, .required)
                 .field(VideoModel.FieldKeys.author, .string, .required)
-                .field(VideoModel.FieldKeys.subtopicId, .uuid, .required, .references(SubtopicModel.schema, .id))
-//                .foreignKey(VideoModel.FieldKeys.subtopicId,
-//                        references: SubtopicModel.schema, SubtopicModel.FieldKeys.subtopicId,
-//                        onDelete: .cascade,
-//                        onUpdate: .cascade)
+                .field(VideoModel.FieldKeys.subtopicId, .uuid, .required)
+                .foreignKey(VideoModel.FieldKeys.subtopicId,
+                            references: SubtopicModel.schema, .id,
+                        onDelete: .cascade,
+                        onUpdate: .cascade)
                 .create(),
         ])
         
